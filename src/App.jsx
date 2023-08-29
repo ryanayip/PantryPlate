6c842bf0ea454111af8d5caf8a135ee2d19bb574
ryip94 <ryip94@yahoo.com>
2023-08-29 05:43:53 -0700

    Added Google font for logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Container from "react-bootstrap/Container";
 function App() {
   return (
     <>
-      <Container>Hello, Pantry</Container>
+      <Container>Hello, PantryPlate</Container>
     </>
   );
 }
